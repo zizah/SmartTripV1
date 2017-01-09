0b69e32d092aa79976a62a85eb4bf73072b161e1
zizah <zizahsimoo@gmail.com>
2017-01-09 11:22:11 +0100

    manage files

---
+++ SmartTripWebClient/SmartTripWebClient/Models/DAL/Dal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartTripWebClient.Models.DAL
+{
+    public class Dal : IDal
+    {
+        public T_E_ABONNE_ABO AuthentifierAbonne(string nom, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SmartTripWebClient/SmartTripWebClient/Models/DAL/IDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTripWebClient.Models.DAL
+{
+    public interface IDal : IDisposable
+    {
+
+
+        T_E_ABONNE_ABO AuthentifierAbonne(string nom, string password);
+
+
+    }
+}
