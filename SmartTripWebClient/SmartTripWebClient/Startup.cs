72734e21023e535cc1eb426d2cee7eb338b0c3cb
zizah <zizahsimoo@gmail.com>
2017-01-09 10:26:38 +0100

    Init WebClient

---
+++ SmartTripWebClient/SmartTripWebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartTripWebClient.Startup))]
+namespace SmartTripWebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
